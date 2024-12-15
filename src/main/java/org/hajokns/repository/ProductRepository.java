f8cf82c2dc0ec77e22c2eeccd70276e8710a258e
HajokNS <c.chornopyskyi.denys@student.uzhnu.edu.ua>
2024-12-15 17:12:27 +0200

    Added Spring WEB REST API

---
+++ src/main/java/org/hajokns/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package org.hajokns.repository;
+
+import java.util.Optional;
+import org.hajokns.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findProductByTitle(String title);
+
+    Page<Product> findByPriceBetween(Double minPrice, Double maxPrice, Pageable pageable);
+}
---
+++ src/test/java/org/hajokns/ProductsSpringWebDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hajokns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsSpringWebDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
